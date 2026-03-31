d36cccda4d80d68f74d973be63b9f1090c7d9182
Cosmo <engs-pierrecoelho@camporeal.edu.br>
2026-03-31 18:59:57 -0300

    Iniciando as variaveis

--- sombras&ossos.cpp
+++ sombras&ossos.cpp
@@ -5,5 +5,15 @@ int main() {
     // Criação das variaveis
     int vida, escolha;
     bool temTocha,temChave, temAmuleto, temEspada;
+    
+    //Inicialização das variaveis
+    vida = 0;
+    escolha = 0;
+    temTocha = false;
+    temChave = false;
+    temAmuleto = false;
+    temEspada = false;
+
+    
     return 0;
 }
