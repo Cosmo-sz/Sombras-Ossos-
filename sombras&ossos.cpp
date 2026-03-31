2ebd914a91b33e9a1dc00c36ac155e93db169097
Cosmo <engs-pierrecoelho@camporeal.edu.br>
2026-03-31 19:01:13 -0300

    criação do personagem

--- sombras&ossos.cpp
+++ sombras&ossos.cpp
@@ -14,6 +14,32 @@ int main() {
     temAmuleto = false;
     temEspada = false;
 
-    
+    // Criação do personagem
+    cout << "==============================" << endl;
+    cout << "        Bem Vindo Jogador!" << endl;
+    cout << "Voce esta em uma aventutura em busca" << endl;
+    cout << "de um tesouro perdido, mas cuidado," << endl;
+    cout << "o caminho esta cheio de perigos!" << endl;
+    cout << "==============================" << endl;
+    cout << "Para dar inicio a sua jornada" << endl;
+    cout << "escolha seu personagem" << endl;
+    cout << "[0] Guerreiro (Vida = 120 e espada)" << endl;
+    cout << "[1] Aventureiro (vida = 100 e Tocha)" << endl;
+    cin >> escolha;
+    switch (escolha)
+    {
+        case 0:
+            vida = 120;
+            temEspada = true;
+            break;
+        case 1:
+            vida = 100;
+            temTocha = true;
+            break;
+        default:
+            cout << "Escolha Invalida, tente novamente!" << endl;
+            break;
+
+    }
     return 0;
 }
