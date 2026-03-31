a2f209bf0a2fc5366f5b7f88f4e0c5cdd132a119
Cosmo <engs-pierrecoelho@camporeal.edu.br>
2026-03-31 18:58:34 -0300

    Criação das variaveis

---
+++ sombras&ossos.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    // Criação das variaveis
+    int vida, escolha;
+    bool temTocha,temChave, temAmuleto, temEspada;
+    return 0;
+}
